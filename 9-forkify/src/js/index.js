2fd2607de6efeb5e76f9f904272c88088e5cbbbd
papashep <melvyn.shepherd@outlook.com>
2020-10-01 15:02:06 +0100

    Section 9 Install node.js and npm, webpack, webpack-cli, jquery.

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,1 +1,16 @@
+/***********************************************************************************************************************
+ * MODERN JAVASCRIPT; USING ES6, NPM, BABEL AND WEBPACK
+ *
+ * Modern JavaScript is not so much about the language itself, but it more about the eco system that we use to write
+ * it in.
+ * We still write the exact same JavaScript code, but we use it together with a set of tools that make it easier and
+ * better to work with and the foundation of all these tools is the NODE.JS and NPM eco system where we can find all
+ * kinds of third party open source tools, libraries and frameworks development tools need for modern web development.
+ * Libraries and Frameworks we have things like React, Angular, jquery Lodash etc, we also have Developments tools like
+ * for task automations, automatic browser reloading or to compare Javascript from ES6 down to ES5.
+ * In order to use and share these packages we need some kind og toll to install and manage them and that's when
+ * the Node Package Manager (NPM) comes it which is a simple command line tool. NPM also allows us to write scripts
+ * for use with development tools. See Notes.docx in this section.
+ ***********************************************************************************************************************
+ */
 // Global app controller
