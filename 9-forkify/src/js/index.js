a7fa04ce52d3e26e526ed5a52f6c21235ec4bbb6
papashep <melvyn.shepherd@outlook.com>
2020-10-05 17:05:50 +0100

    Section 9 Configuration setup for Webpack and Babel now completed, ready to start the project.

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -10,10 +10,7 @@
  * for task automations, automatic browser reloading or to compare Javascript from ES6 down to ES5.
  * In order to use and share these packages we need some kind og toll to install and manage them and that's when
  * the Node Package Manager (NPM) comes it which is a simple command line tool. NPM also allows us to write scripts
- * for use with development tools. See Notes.docx in this section.
+ * for use with development tools. See course notes 'Notes.docx' for this section.
  ***********************************************************************************************************************
  */
 // Global app controller
-import num from './test';
-console.log(`I imported ${num} from another module!`);
-
